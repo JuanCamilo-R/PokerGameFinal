84e5bfcec36e3da96f052b1500de9345bd5348c5
jennyferbm13 <51867672+jennyferbm13@users.noreply.github.com>
2020-11-05 12:33:25 -0500

    logo

--- src/pokerVista/MesaJuego.java
+++ src/pokerVista/MesaJuego.java
@@ -2,14 +2,19 @@ package pokerVista;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.util.List;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import pokerModelo.Carta;
 
 public class MesaJuego extends JPanel {
 	private PanelJugador panelJugador1,panelJugador2,panelJugador3,panelJugador4,panelJugador5;
+	private ImageIcon imagen;
+	private JLabel logo;
 	
 	private GridBagConstraints constraints;
 	
@@ -46,6 +51,16 @@ public class MesaJuego extends JPanel {
 			constraints.gridheight =1;
 			constraints.fill = constraints.HORIZONTAL;
 			add(panelJugador3, constraints);
+			
+			logo = new JLabel();
+			imagen = new ImageIcon(getClass().getResource("/resources/logo.png"));
+			logo.setIcon(new ImageIcon(imagen.getImage().getScaledInstance(150,150, Image.SCALE_DEFAULT)));
+			constraints.gridx =2;
+			constraints.gridy =0;
+			constraints.gridwidth =1;
+			constraints.gridheight =1;
+			constraints.fill = constraints.WEST;
+			add(logo, constraints);
 			
 		}
 }
